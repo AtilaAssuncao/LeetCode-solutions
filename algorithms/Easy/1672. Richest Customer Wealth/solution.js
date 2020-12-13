7096fae32f49a3f619fa9804df8602995f1483f7
repl.it user <replituser@example.com>
2020-12-13 02:29:07 +0000

    add

---
+++ algorithms/Easy/1672. Richest Customer Wealth/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let aux = 0;
+    for (let i = 0; i < accounts.length; i++) {
+        let count = 0;
+        for (let j = 0; j < accounts[i].length; j++) {
+            count += accounts[i][j];
+        }
+        if( count > aux ) aux = count;
+    }
+    return aux;
+};
